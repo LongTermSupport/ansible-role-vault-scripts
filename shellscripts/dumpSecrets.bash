b976fed871ba890ea4bb1d4d353aa3fb102b20b0
joseph <joseph@joseph-home>
2020-12-16 15:01:25 +0000

    dump secrets script, dump secrets task as role task, working nicely

---
+++ shellscripts/dumpSecrets.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+readonly scriptDir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+cd "$scriptDir"
+# Set up bash
+source ./_top.inc.bash
+
+
+# Usage
+if (($# < 1 )); then
+  echo "
+  Usage:
+
+  This script will dump all the secrets in the specifiedEnv
+
+  $(basename $0) [specifiedEnv]
+
+  "
+  exit 1
+fi
+
+readonly specifiedEnv="$1"
+
+cd "$projectDir"
+
+ansible localhost -m import_role -a name=vault-scripts --extra-vars env_dir="$projectDir/environment/$specifiedEnv"

9407ccdb28dda5deb342e5113583ec640cb65469
Joseph Edmonds <joseph@ltscommerce.dev>
2023-07-18 12:28:11 +0100

    create pre shared key script

---
+++ shellscripts/createVaultedPreSharedKey.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+readonly scriptDir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd -P)"
+cd "$scriptDir"
+# Set up bash
+source ./_top.inc.bash
+
+# Usage
+if (($# < 1 || $# > 3)); then
+  echo "
+  Usage:
+
+  This script will generate a pre shared key and encrypt it, then optionally add it to the file you specify
+
+  Please note, the varname must be prefixed with 'vault_'
+
+  $(basename $0) [varname] (optional: outputToFile) (optional: specifiedEnv - defaults to $defaultEnv)
+
+
+
+  "
+  exit 1
+fi
+
+# Set variables
+readonly varname="$1"
+outputToFile="$(getProjectFilePathCreateIfNotExists "${2:-}")"
+readonly specifiedEnv="${3:-$defaultEnv}"
+
+psk="$(openssl rand -hex 32)"
+
+# Source vault top
+source ./_vault.inc.bash
+
+# Assertions
+assertValidEnv "$specifiedEnv"
+assertPrefixedWithVault "$varname"
+readonly prefixed_varname="$varname"
+validateOutputToFile "$outputToFile" "$varname"
+
+# Create vault string
+encrypted="$(echo -n "$psk" | ansible-vault encrypt_string \
+  --vault-id="$specifiedEnv@$vaultSecretsPath" \
+  --stdin-name "$prefixed_varname")"
+
+writeEncrypted "$encrypted" "$prefixed_varname" "$outputToFile"
